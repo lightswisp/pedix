8289ec6cf5405f0d39d243d8017ebf4d14169c38
lightswisp <48927861+lightswisp@users.noreply.github.com>
2024-08-27 01:56:24 -0400

    fixed tests

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 INCLUDE_DIR=include
 SRC_DIR=src
 OBJ_DIR=build
+TEST_DIR=test
+TEST_OBJ_DIR=build_$(TEST_DIR)
 
 OUT=main.exe
 DEBUG_FLAGS=-ggdb
@@ -9,6 +11,9 @@ CC=gcc
 
 SRCS := $(shell find $(SRC_DIR) -name "*.c")
 OBJS := $(SRCS:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
+
+TEST_SRCS := $(shell find $(SRC_DIR) $(TEST_DIR) -name "*.c" ! -name "main.c") 
+TEST_OBJS := $(patsubst %.c, $(TEST_OBJ_DIR)/%.o, $(notdir $(TEST_SRCS)))
 
 all: pedix 
 
@@ -19,10 +24,29 @@ $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
 	@mkdir -p $(@D)
 	$(CC) $(CFLAGS) -c $< -o $@
 
-.PHONY: run all clean
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(TEST_OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(TEST_OBJ_DIR)/test.o: $(TEST_DIR)/test.c
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+.PHONY: run all clean test
 
 clean:
 	rm $(OBJ_DIR)/*.o
+	rmdir $(OBJ_DIR)
+
+test: $(TEST_OBJS)
+	$(CC) $^ -o test_$(OUT) && ./test_$(OUT)
+	@rm $(TEST_OBJ_DIR)/*.o
+	@rmdir $(TEST_OBJ_DIR)
+	@rm test_$(OUT)
 
 run: 
 	./$(OUT)
--- test/test.c
+++ test/test.c
@@ -1,6 +1,5 @@
-#include "../src/headers/decoder.h"
-#include "../src/headers/operand.h"
-#include "../src/headers/mnemonic.h"
+#include <pedix/decoder.h>
+#include <pedix/dump.h>
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
@@ -9,21 +8,13 @@
 #define GREEN "\033[0;32m"
 #define NC "\033[0m"
 #define TEXT_IDENTICAL(d, s)((strcmp(d,s) == 0))
-#define FORMAT_INSTRUCTION(d, m, o)(sprintf(d, "%.24s %.24s", m, o))
 
 /* test template */
 #define PREPARE_TEST(n, itext)                                                 \
-  assert(("Decode   test " #n && decode(decoded, instruction##n) == true));    \
+  pedix_decode(decoded, instruction##n);                                       \
   assert(                                                                      \
-      ("Mnemonic test " #n && set_mnemonic(decoded, decoded->op1) == true));   \
-  assert(                                                                      \
-      ("Operands test " #n && set_operands(decoded, decoded->op1) == true));   \
-  FORMAT_INSTRUCTION(temp, decoded->mnemonic.str, decoded->operands.str);      \
-  printf("temp: %s\n", temp); \
-  assert(("Instruction text test " #n && TEXT_IDENTICAL(temp, itext)));        \
-  print_bytes(decoded);                                                        \
-  print_instruction(decoded);                                                  \
-  zero_instruction(decoded);
+      ("instruction text test " #n && TEXT_IDENTICAL(decoded->text, itext)));  \
+  pedix_zero_instruction(decoded);
 
 /*add WORD PTR[ecx+esi*1],cx */
 uint8_t instruction1[] = {0x66, 0x01, 0x0C, 0x31}; 
@@ -39,25 +30,10 @@ uint8_t instruction3[] = {
 /* mov DWORD PTR [ecx+edx*2+0x50],0x11111111 */
 uint8_t instruction4[] = {0xC7, 0x44, 0x51, 0x50, 0x11, 0x11, 0x11, 0x11};       
 
-void print_bytes(Dinstruction *decoded){
-  for (size_t i = 0; i < decoded->buffer.size; i++) {
-    printf("0x%02X ", decoded->buffer.bytes[i]);
-  }
-}
-
-void print_instruction(Dinstruction *decoded) {
-  if (decoded->operands.capacity == 0)
-    printf("%s\n", decoded->mnemonic.str);
-  else
-    printf("%s %s\n", decoded->mnemonic.str, decoded->operands.str);
-}
-
 int main(){
   /* init */
-  Dinstruction *decoded = init_instruction();
+  decoded_instruction_t *decoded = pedix_init_instruction();
   decoded->mode = MODE_32;
-  char temp[50];
-
 
   /* tests */
   PREPARE_TEST(1, "add WORD PTR [ecx+esi*1],cx");
