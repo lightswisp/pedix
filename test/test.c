1e826c171dff6d645dcd321789ba8ae9b1668b9c
lightswisp <48927861+lightswisp@users.noreply.github.com>
2024-09-06 06:18:29 -0400

    fldenv

--- test/test.c
+++ test/test.c
@@ -113,6 +113,7 @@ PREPARE_INSTRUCTION(79, "\xd9\xce");
 PREPARE_INSTRUCTION(80, "\xD9\x94\x10\x20\x30\x40\x50");
 PREPARE_INSTRUCTION(81, "\xD9\xD0");
 PREPARE_INSTRUCTION(82, "\xD9\xDF");
+PREPARE_INSTRUCTION(83, "\xD9\xA2\x11\x22\x33\x44");
 
 int main(void){
   decoded_instruction_t *decoded = pedix_init_instruction();
@@ -201,6 +202,7 @@ int main(void){
   PREPARE_TEST(80, "fst DWORD PTR [eax+edx*1+0x50403020]");
   PREPARE_TEST(81, "fnop");
   PREPARE_TEST(82, "fstp1 st7");
+  PREPARE_TEST(83, "fldenv DWORD PTR [edx+0x44332211]");
 
   puts(GREEN"All tests are passed!"NC);
   return 0;
