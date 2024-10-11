2f695a664ec8ad6c3cde3626de0fd17bc10db77b
lightswisp <48927861+lightswisp@users.noreply.github.com>
2024-10-11 06:11:16 -0400

    Update dump.h

--- include/pedix/dump.h
+++ include/pedix/dump.h
@@ -2,5 +2,6 @@
 #include "decoder.h"
 #include "tables/tabledef.h"
 
-void pedix_dump_instruction(instruction_t *decoded);             
+void pedix_dump_instruction(instruction_t *decoded);     
+void pedix_dump_memory(uint8_t* addr, size_t size);
 void pedix_print_instruction(decoded_instruction_t *decoded, uint64_t offset);  
